d1696e953b91f1fa0fb5bffcab4244eb1eb22d91
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-09-23 14:12:01 +0530

    scale the app to more users

--- registration.js
+++ registration.js
@@ -12,8 +12,13 @@
 // storing user data on localstorage in the form of objects
 let btn=document.querySelector('.fbtn');
 let form=document.querySelector('#form');
+let ul=document.querySelector('#ul')
 btn.addEventListener('click',()=>{
     var datas = document.querySelectorAll(".inputs")
+    let li=document.createElement('li');
+    li.innerText=`${datas[0].value}  ${datas[1].value} ${datas[2].value}`
+    ul.appendChild(li);
+    li.style.color='white';
     let users={
         name:datas[0].value,
         email:datas[1].value,
@@ -21,7 +26,7 @@ btn.addEventListener('click',()=>{
 
     }
    let modifyUsers=JSON.stringify(users);
-    localStorage.setItem('users',modifyUsers);
+    localStorage.setItem(datas[1].value,modifyUsers);
 })
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
