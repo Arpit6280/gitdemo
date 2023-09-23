7c5a3442581d4c0c4e875a2fd5e0e3653b01c4bf
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-09-23 13:48:46 +0530

    store the user data in right format

--- registration.js
+++ registration.js
@@ -1,11 +1,27 @@
 
+// let btn=document.querySelector('.fbtn');
+// let form=document.querySelector('#form');
+// btn.addEventListener('click',()=>{
+//     var datas = document.querySelectorAll(".inputs")
+//     localStorage.setItem('name',datas[0].value);
+//     localStorage.setItem('email',datas[1].value);
+//     localStorage.setItem('phone No.',datas[2].value);
+// })
+
+
+// storing user data on localstorage in the form of objects
 let btn=document.querySelector('.fbtn');
 let form=document.querySelector('#form');
 btn.addEventListener('click',()=>{
     var datas = document.querySelectorAll(".inputs")
-    localStorage.setItem('name',datas[0].value);
-    localStorage.setItem('email',datas[1].value);
-    localStorage.setItem('phone No.',datas[2].value);
+    let users={
+        name:datas[0].value,
+        email:datas[1].value,
+        phoneNo:datas[2].value
+
+    }
+   let modifyUsers=JSON.stringify(users);
+    localStorage.setItem('users',modifyUsers);
 })
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
