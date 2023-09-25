76bb7535cfdbdf9f731662b448aaeebdb34e6517
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-09-25 06:29:54 +0530

    edit button

--- registration.js
+++ registration.js
@@ -16,12 +16,16 @@ let ul=document.querySelector('#ul')
 btn.addEventListener('click',()=>{
     var datas = document.querySelectorAll(".inputs")
     let button=document.createElement('button');
+    let editBtn=document.createElement('button');
+    editBtn.id='ebtn';
+    editBtn.innerText='Edit';
     button.id='btn';
     button.innerText='Delete'
     let li=document.createElement('li');
     li.innerText=`${datas[0].value}  ${datas[1].value} ${datas[2].value}  `
     ul.appendChild(li);
     ul.appendChild(button);
+    ul.appendChild(editBtn)
     ul.appendChild(document.createElement('br'))
     li.style.color='white';
     li.style.display='inline';
@@ -36,9 +40,24 @@ btn.addEventListener('click',()=>{
 
    // delete button functionality
    button.addEventListener('click',(e)=>{
+    localStorage.removeItem(users.email);
     ul.removeChild(li);
     ul.removeChild(button)
+    ul.removeChild(editBtn);
 })
+
+
+  //edit Button functionality
+  editBtn.addEventListener('click',()=>{
+    ul.removeChild(li);
+    ul.removeChild(button);
+    ul.removeChild(editBtn);
+   
+    datas[0].value=users.name;
+    datas[1].value=users.email;
+    datas[2].value=users.phoneNo;
+    localStorage.removeItem(users.email);
+  })
    
 })
 form.addEventListener('submit',(e)=>{
