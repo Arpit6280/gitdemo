f89341a3f6fb26ee7fd1930f28c0fab2c7a13997
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-09-25 05:41:27 +0530

    delete data from localstorage

--- registration.js
+++ registration.js
@@ -15,10 +15,16 @@ let form=document.querySelector('#form');
 let ul=document.querySelector('#ul')
 btn.addEventListener('click',()=>{
     var datas = document.querySelectorAll(".inputs")
+    let button=document.createElement('button');
+    button.id='btn';
+    button.innerText='Delete'
     let li=document.createElement('li');
-    li.innerText=`${datas[0].value}  ${datas[1].value} ${datas[2].value}`
+    li.innerText=`${datas[0].value}  ${datas[1].value} ${datas[2].value}  `
     ul.appendChild(li);
+    ul.appendChild(button);
+    ul.appendChild(document.createElement('br'))
     li.style.color='white';
+    li.style.display='inline';
     let users={
         name:datas[0].value,
         email:datas[1].value,
@@ -26,7 +32,14 @@ btn.addEventListener('click',()=>{
 
     }
    let modifyUsers=JSON.stringify(users);
-    localStorage.setItem(datas[1].value,modifyUsers);
+   localStorage.setItem(datas[1].value,modifyUsers);
+
+   // delete button functionality
+   button.addEventListener('click',(e)=>{
+    ul.removeChild(li);
+    ul.removeChild(button)
+})
+   
 })
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
